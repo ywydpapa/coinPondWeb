b728b005b3e16bd548b1d4c9fa429e42565439c6
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2024-03-20 16:37:31 +0900

    거래설정3

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def logout():
     return render_template('./login/login.html')
 
 if __name__ == '__main__':
-    app.run( debug=True, host='0.0.0.0', port='5000' )
+    app.run( debug=True, host='0.0.0.0', port=5000)
