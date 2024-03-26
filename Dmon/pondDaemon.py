e14bc692bc3cca56a4cbbd6e306750c7d2d063ab
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2024-03-26 16:08:13 +0900

    거래내역 수정

--- Dmon/pondDaemon.py
+++ Dmon/pondDaemon.py
@@ -19,6 +19,16 @@ def getorders(key1, key2, coinn):
     return orders
 
 
+def liqcoin(key1, key2, coinn): #코인 청산
+    upbit = pyupbit.Upbit(key1,key2)
+    orders = upbit.get_order(coinn)
+    if len(orders)>0:
+        for order in orders:
+            upbit.cancel_order(order["uuid"])
+    else:
+        pass
+
+
 def runorders():
     setons = comm.dbconn.getseton()
     if setons is not None:
@@ -26,11 +36,25 @@ def runorders():
             keys = getkeys(seton)
             myset = loadmyset(seton)
             items = getorders(keys[0], keys[1], myset[6])
-            if myset is not None:
-                print(myset)
-                print(items)
+            print(myset)
+            if myset[7] == 'Y':
+                iniAsset = myset[2]
+                interVal = myset[3]
+                intergap = myset[4]
+                interestRate = myset[5]
+                coinn = myset[6]
+                preprice = pyupbit.get_current_price(coinn)
+                print('현재가', preprice)
+                bidasset = iniAsset
+                for i in range(1,interVal+1):
+                    bidprice = ((preprice * 100) - (preprice * intergap*i)) / 100
+                    bidasset = bidasset * 2
+                    print('interval ',i,'예약 거래 적용')
+                    print('매수가격',bidprice)
+                    print('매수금액',bidasset)
+                print('거래 개시')
             else:
-                print("No seton found!")
+                print("거래 대기")
     else:
         print("No seton found!!")
     comm.dbconn.clearcache()
