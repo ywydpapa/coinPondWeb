5237cec5c42483c2605c5cc9d175a72c48710c26
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2024-07-05 02:06:25 +0900

    조회 조건 추가

--- comm/upbitdata.py
+++ comm/upbitdata.py
@@ -9,3 +9,8 @@ from pandas import DataFrame
 def dashcandle548(coinn):
     candles: DataFrame | None = pyupbit.get_ohlcv(coinn, interval="minute5", count=48)
     return candles
+
+
+def dashcandle160(coinn):
+    candles: DataFrame | None = pyupbit.get_ohlcv(coinn, interval="minute1", count=60)
+    return candles
