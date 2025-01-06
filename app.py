6c56dd4ce750d28af86c063f7a3bb8392a0a36da
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2025-01-06 14:57:17 +0900

    부분 매도 수정

--- app.py
+++ app.py
@@ -623,7 +623,7 @@ def updatemyuser():
 def sellcoin():
     pla = request.get_data().decode('utf-8').split(',')
     uno = pla[0]
-    coinn = pla[1]
+    coinn = "KRW-"+pla[1]
     rate = pla[2]
     sellmycoinpercent(uno, coinn, rate)
     return "YES"
